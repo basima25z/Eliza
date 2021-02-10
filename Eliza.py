46a95ebbf33042de30f179c5bfac46b1c6b3c0bc
Basima Zafar <basimazafar@Basimas-MacBook-Pro.local>
2021-02-09 22:02:21 -0500

    commited

--- Eliza.py
+++ Eliza.py
@@ -1,9 +1,15 @@
 import re
 
 def eliza_prompt():
-    print('Hello, my name is Eliza, your therapist. What is your name?')
-    name = input('> ')
-    print(name)
+    # print('Hello, my name is Eliza, your therapist. What is your name?')
+    # name = input('> ')
+    # print(name)
+
+    sentance_to_search='My name is cassie'
+    pattern = re.compile(r'[Mm]y name is (\w+)')
+
+    matches = pattern.sub(r'\1', sentance_to_search)
+    print(matches)
 
 
 if __name__=="__main__":
