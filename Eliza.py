ea690083c3f516110b75c7183a6454a4ee68eb75
Basima Zafar <basimazafar@Basimas-MacBook-Pro.local>
2021-02-16 01:25:13 -0500

    commented code

--- Eliza.py
+++ Eliza.py
@@ -1,5 +1,17 @@
+#####################################
+# Basima Zafar
+# NLP Program 1
+# 2/16/21
+# V00825375
+#####################################
 import re
 
+
+#####################################
+# -- Regex listed below, most of them have [] with the first letter to account for the first letter 
+# of each sentance being either uppercase or lowercase
+# -- (.*): any whitespace or character as long as it's not a new line
+######################################
 regex=[
     (re.compile(r"[Mm]y name is (\w+)"), r"Hi \0, how are you feeling today?"),
     (re.compile(r"[Ii] feel ([\w\s]+)"), r"Do you feel \0 often?"),
@@ -38,8 +50,16 @@ regex=[
     (re.compile(r"[Ii]t is (.*)"), r"Why is that \0?"),
     (re.compile(r"[Ii]t\'?s (.*)"), r"Why is that its \0?")
     ]
-        
 
+
+#################################
+# Function called eliza()
+# First ouputs introduction prompt which the user will respond to
+# s which is type string is empty, when s is not equal to exit will continue to get input from user
+# When exit is entered --> exits program 
+# calls method response which will generate responses from Eliza
+###############################
+        
 def eliza():
     print('My name is Eliza, your personal therapist, what is your name?')
     s = ''
@@ -48,7 +68,20 @@ def eliza():
             s = input('>>> ')
         except EOFError:
             s = 'exit'
+            
+
         print(responses(s))
+
+##############################
+# Function called repsponses
+# Paramter: accepts a string
+# Uses a for loop to traverse through dictionary
+# key is what the input should match (regex), value is what eliza responds
+# if the key(regex) matches the userInput, check if the key matches the groups, if so
+# set it to the variable match
+# if there is a match, then replace the \0 with the match found
+# if there isn't a match, then respond with the value (no replacing)
+##############################
 
 def responses(userInput):
     for key,value in regex:
